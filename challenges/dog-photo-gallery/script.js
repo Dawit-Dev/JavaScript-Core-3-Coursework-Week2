7dad8af2dbdf18f5201401598c6fab041ddbad5a
Dawit Abraha <akdawit1@gmail.com>
2022-10-21 22:14:26 +0100

    Modified dog-photo-gallery

--- challenges/dog-photo-gallery/script.js
+++ challenges/dog-photo-gallery/script.js
@@ -1,25 +1,37 @@
-const parentContainer = document.getElementById("photoContainer");
-const dogPhotoAdder = document.getElementById("btnAdd");
-function dogPhotos() {
-  fetch(`https://dog.ceo/api/breeds/image/random`)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      let dogImageList = document.createElement("li");
+let imagesContainer = document.querySelector(".images-container");
+let addBtn = document.querySelector(".add-btn");
+let deleteBtn = document.querySelector(".delete-btn");
+let url = "https://dog.ceo/api/breeds/image/random";
 
-      let dogImage = document.createElement("img");
-      dogImageList.className = "ImageList";
-      dogImage.style.width = "220px";
-      dogImage.style.height = "200px";
-      dogImage.src = data.message;
-      dogImageList.appendChild(dogImage);
-      parentContainer.appendChild(dogImageList);
-      console.log(dogImage);
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response.json();
+}
+
+function getImage() {
+  let itemElt = document.createElement("li");
+  let imgElt = document.createElement("img");
+  itemElt.className = "image-item";
+  imgElt.className = "image";
+  fetch(url)
+    .then((response) => handleErrors(response))
+    .then((data) => {
+      imgElt.src = data.message;
+      itemElt.append(imgElt);
+      imagesContainer.append(itemElt);
     })
     .catch(function (error) {
       console.log(error);
     });
 }
-dogPhotoAdder.addEventListener("click", dogPhotos);
-window.onload = dogPhotos;
+
+function deleteImage() {
+  let images = document.querySelectorAll(".image-item");
+  let lastImg = images[images.length - 1];
+  imagesContainer.removeChild(lastImg);
+}
+
+addBtn.addEventListener("click", () => getImage());
+deleteBtn.addEventListener("click", () => deleteImage());
